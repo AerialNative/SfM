c3d5393d7adbeb8799f68377ae793fa88eaac75e
NAP612 <native.aerial.productions@gmail.com>
2020-10-27 00:11:34 -0600

    Sanbartolo

---
+++ pointclouds/Sanbartolo/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 66774329,
+    "boundingBox": {
+        "lx": -546.9010000000001,
+        "ly": -409.679,
+        "lz": 356.869,
+        "ux": 125.76099999999997,
+        "uy": 262.98300000000008,
+        "uz": 1029.531
+    },
+    "tightBoundingBox": {
+        "lx": -546.9010000000001,
+        "ly": -409.679,
+        "lz": 356.869,
+        "ux": 125.76099999999997,
+        "uy": 95.10200000000009,
+        "uz": 463.738
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 5.825423717498779,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
